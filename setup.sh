fb39571ca01349ced05a3718f48d873e6585a92a
Fizzl <fizzl@fizzl.net>
2026-04-29 11:30:25 +0300

    lz4

--- setup.sh
+++ setup.sh
@@ -61,7 +61,7 @@ install_host_packages() {
         gawk wget git diffstat unzip texinfo gcc build-essential chrpath
         socat cpio python3 python3-pip python3-pexpect xz-utils debianutils
         iputils-ping python3-git python3-jinja2 python3-subunit zstd
-        liblz4-tool file locales libacl1
+        lz4 file locales libacl1
     )
 
     log "Updating apt package index..."
